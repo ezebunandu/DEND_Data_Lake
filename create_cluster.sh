064d62e189ccd4e1f5baaae4103179bf86653049
Sam Ezebunandu <samuel.ezebunandu@outlook.com>
2020-09-15 20:37:55 -0600

    Bash script for creating the EMR cluster

---
+++ create_cluster.sh
@@ -0,0 +1,10 @@
+#! bin/zsh
+aws emr create-cluster \
+--name data-lake-cluster \
+--use-default-roles \
+--release-label emr-5.28.0 \
+--instance-count 3 \
+--applications Name=Spark \
+--ec2-attributes KeyName=spark-cluster \
+--instance-type m5.xlarge \
+--log-uri s3://s3-for-spark-cluster/
